acbffcfce88823fa4941f831e6516f951693713f
Paul Schlueter <ps589@ntrs.com>
2024-02-29 14:18:11 -0600

    Test Pipeline

--- src/main_test.py
+++ src/main_test.py
@@ -2,4 +2,4 @@ from main import duplicate_string
 
 
 def test_duplicate_string_should_duplicate_string_8_times_when_number_is_8():
-    assert duplicate_string("test", 8) == "test" * 8
+    assert duplicate_string(input_str="test", num_repetitions=8) == "test" * 8
