e9c1f865cbe2864229783fbf8c3114c878d619d7
Paul Schlueter <ps589@ntrs.com>
2024-02-29 13:03:37 -0600

    Uncomment printf

--- scripts/checks.sh
+++ scripts/checks.sh
@@ -1,10 +1,10 @@
 # exit if any command fails
 set -e
 
-# printf "Formatting Check\n"
+printf "Formatting Check\n"
 black src --check
 
-# printf "\nTyping Check\n"
+printf "\nTyping Check\n"
 mypy src
 
 printf "\nLint Check\n"
